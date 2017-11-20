ea096fb7db95f0d1f3280a61f91e7a9154576aa3
Aashish Chaudhary <aashish24@gmail.com>
2017-11-20 08:59:15 -0500

    Added python pdal

--- recipes/pdal/build.sh
+++ recipes/pdal/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # pushd swig/python
+cd python
 
 $PYTHON setup.py build_ext \
-    --include-dirs $INCLUDE_PATH \
-    --library-dirs $LIBRARY_PATH
+    --include-dirs $PREFIX/include \
+    --library-dirs $$PREFIX/lib
 # $PYTHON setup.py build_py
 # $PYTHON setup.py build_scripts
 $PYTHON setup.py install

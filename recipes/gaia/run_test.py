4930645aeea821287614f60d3acca50dbc3b3ada
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-01-04 16:22:25 -0700

    Improve package install test

--- recipes/gaia/run_test.py
+++ recipes/gaia/run_test.py
@@ -1,5 +1,30 @@
-import os
+import os, unittest
 import gaia
+import geopandas
 
-fname = os.path.join(os.environ['RECIPE_DIR'], 'test_data', 'test.shp')
-# geopandas.read_file(fname)
+
+class TestGaiaInstall(unittest.TestCase):
+
+    def test_read_file(self):
+        fname = os.path.join(os.environ['RECIPE_DIR'], 'test_data', 'test.shp')
+        df = geopandas.read_file(fname)
+        bounds = df.total_bounds
+        self.assertEqual(len(bounds), 4)
+
+        # >>> df.total_bounds
+        # array([-82.83,  25.35, -75.52,  36.18])
+
+        self.assertGreater(bounds[0], -83)
+        self.assertLess(bounds[0], -82)
+
+        self.assertGreater(bounds[1], 25)
+        self.assertLess(bounds[1], 26)
+
+        self.assertGreater(bounds[2], -76)
+        self.assertLess(bounds[2], -75)
+
+        self.assertGreater(bounds[3], 36)
+        self.assertLess(bounds[3], 37)
+
+if __name__ == '__main__':
+    unittest.main()
